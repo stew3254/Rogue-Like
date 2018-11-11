77c477899830d236e24fb4c9a683c7e98d1a620b
thajohns <0.442.26622.7tj@gmail.com>
2018-11-11 09:26:01 -0500

    Added comments to world.py

--- world.py
+++ world.py
@@ -2,36 +2,44 @@
 
 import math
 import enum
+import random
 
 class Tile(enum.Enum):
   EMPTY = 0
   WALL = 1
 
 class World:
+# Construct a world in a window of pixel dimensions given, seeded
   def __init__(this, width, height, seed):
     this.cx = width * 0.5
     this.cy = height * 0.5
+    this.bx = math.ceil(width / 16)
+    this.by = math.ceil(height / 16)
+    this.level = 0
     print("world constructor called")
 
-  def toMapC(this, x, y):
+  def toMapC(this, x, y):  # Pixel coords -> map coords
     return ((x - this.cx) / 16, (y - this.cy) / 16)
 
-  def fromMapC(this, x, y):
+  def fromMapC(this, x, y):  # Map coords -> pixel coords
     return (x * 16 + this.cx, y * 16 + this.cy)
 
-  def getTileAt(this, x, y):
+  def getTileAt(this, x, y):  # Tile at (x, y) in map coords
     if (math.abs(x) < 5 and math.abs(y) < 5):
       return Tile.EMPTY
     else:
       return Tile.WALL
 
-  def addEnt(ent):
+  def addEnt(ent):  # Put entity on the map
     ents.push(ent)
 
-  def getEnts():
+  def getEnts():  # Get master entity list
     return []
 
   ents = []
+  level = 0
+  grand = random.Random()
+  lrand = random.Random()
   
 
   
