c65645cccf416e792d150a20d71798ad4c093c00
stew3254 <rtstewy@gmail.com>
2018-11-11 12:59:27 -0500

    Added minimal character model

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ import pyxel
 
 class App:
 	def __init__(self):
+		#Initialize the window
 		pyxel.init(255, 255, caption="Rogue Like")
+
+		#Load assets
+		pyxel.load("assets/rogue_like.pyxel")
+
+		#Run the game
 		pyxel.run(self.update, self.draw)
 
 	def update(self):
+		#Quit if 'q' is pressed
 		if pyxel.btnp(pyxel.KEY_Q):
 			pyxel.quit()
 
 	def draw(self):
+		#Clear the screen with black
 		pyxel.cls(0)
-		pyxel.rect(10, 10, 20, 20, 11)
-	
+
+		#Draw the player model
+		pyxel.blt(0, 0, 0, 0, 0, 16, 16)
+
 
 App()
