6287b93218fd939b3b89138640c1ca6a34eaa5e6
stew3254 <rtstewy@gmail.com>
2018-11-11 14:13:43 -0500

    Now draws tiles

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
 import pyxel
+import world
 
 
 class App:
 	def __init__(self):
+		self.screen_width = 255
+		self.screen_height = 255
+
 		#Initialize the window
-		pyxel.init(255, 255, caption="Rogue Like")
+		pyxel.init(self.screen_width, self.screen_height, caption="Rogue Like")
 
 		#Load assets
 		pyxel.load("assets/rogue_like.pyxel")
+
+		#Create world constructor
+		self.derp = world.World(self.screen_width, self.screen_height, 0)
 
 		#Run the game
 		pyxel.run(self.update, self.draw)
@@ -22,6 +29,14 @@ class App:
 	def draw(self):
 		#Clear the screen with black
 		pyxel.cls(0)
+
+		#Draw the background
+		for x in range(int((self.screen_width + 1)/16)):
+			for y in range(int((self.screen_height + 1)/16)):
+
+				#If something is a wall, draw the wall
+				if (self.derp.getTileAt(x, y) == world.Tile.WALL):
+					pyxel.blt(x*16, y*16, 0, 0, 16, 16, 32)
 
 		#Draw the player model
 		pyxel.blt(0, 0, 0, 0, 0, 16, 16)
