c3279c8d9afe4aa57f781e1c2887b2a218a4970b
stew3254 <rtstewy@gmail.com>
2018-11-11 18:33:21 -0500

    Fixed graphics

--- main.py
+++ main.py
@@ -24,11 +24,17 @@ class App:
 		pyxel.run(self.update, self.draw)
 
 	def tryMove(self, x, y):
+		#Get coords
 		(plx, ply) = (self.w.getPlayer().x, self.w.getPlayer().y)
+
+		#If godmode or can move, move
 		if (godmode or world.Tile.isClear(self.w.getTileAt(plx + x, ply + y))):
+			#Update players
 			self.w.getPlayer().x += x
 			self.w.getPlayer().y += y
+
 			return True
+
 		print("That's a wall")
 		return False
 
@@ -36,12 +42,20 @@ class App:
 		#Quit if 'q' is pressed
 		if pyxel.btnp(pyxel.KEY_Q):
 			pyxel.quit()
+
+		#Move up one
 		if pyxel.btnp(pyxel.KEY_W):
 			self.tryMove(0, -1)
+
+		#Move down one
 		if pyxel.btnp(pyxel.KEY_S):
 			self.tryMove(0, 1)
+
+		#Move left one
 		if pyxel.btnp(pyxel.KEY_A):
 			self.tryMove(-1, 0)
+
+		#Move right one
 		if pyxel.btnp(pyxel.KEY_D):
 			self.tryMove(1, 0)
 
@@ -53,17 +67,22 @@ class App:
 		#Draw the background
 		for x in range(int((self.screen_width + 1)/16 + 1)):
 			for y in range(int((self.screen_height + 1)/16 + 1)):
+				#Translate tiles to pixels
 				px = x * 16
 				py = y * 16
 				(bx, by) = self.w.toMapC(px, py)
+
 				#If something is a wall, draw the wall
 				if (self.w.getTileAt(bx, by) == world.Tile.WALL):
 					(px, py) = self.w.getBlockBase(px, py)
-					pyxel.blt(px, py, 0, 0, 16, 16, 32)
+					pyxel.blt(px, py, 0, 16, 16, 16, 16)
+				else:
+					(px, py) = self.w.getBlockBase(px, py)
+					pyxel.blt(px, py, 0, 0, 16, 16, 16)
 
 		#Draw the player model
 		(plx, ply) = self.w.fromMapC(self.w.getPlayer().x, self.w.getPlayer().y)
-		pyxel.blt(plx, ply, 0, 0, 0, 16, 16, 14)
+		pyxel.blt(plx, ply, 0, 0, 0, 16, 16, 2)
 
 
 App()
