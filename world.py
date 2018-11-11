115ec9c02d6677bebcd3b46fefa37adcdce90f5e
thajohns <0.442.26622.7tj@gmail.com>
2018-11-11 10:14:21 -0500

    Fixed math.abs() derp and added world.getBlockBase()

--- world.py
+++ world.py
@@ -10,22 +10,25 @@ class Tile(enum.Enum):
 
 class World:
 # Construct a world in a window of pixel dimensions given, seeded
-  def __init__(this, width, height, seed):
-    this.cx = width * 0.5
-    this.cy = height * 0.5
-    this.bx = math.ceil(width / 16)
-    this.by = math.ceil(height / 16)
-    this.level = 0
+  def __init__(self, width, height, seed):
+    self.cx = width * 0.5
+    self.cy = height * 0.5
+    self.bx = math.ceil(width / 16)
+    self.by = math.ceil(height / 16)
+    self.level = 0
     print("world constructor called")
 
-  def toMapC(this, x, y):  # Pixel coords -> map coords
-    return ((x - this.cx) / 16, (y - this.cy) / 16)
+  def toMapC(self, x, y):  # Pixel coords -> map coords
+    return ((x - self.cx) / 16, (y - this.cy) / 16)
 
-  def fromMapC(this, x, y):  # Map coords -> pixel coords
-    return (x * 16 + this.cx, y * 16 + this.cy)
+  def fromMapC(self, x, y):  # Map coords -> pixel coords
+    return (x * 16 + self.cx, y * 16 + this.cy)
 
-  def getTileAt(this, x, y):  # Tile at (x, y) in map coords
-    if (math.abs(x) < 5 and math.abs(y) < 5):
+  def getBlockBase():
+    return ((x - self.cx) - (x - self.cx) % 16 + self.cx, (y - self.cx) - (y - self.cx) % 16 + self.cx)
+
+  def getTileAt(self, x, y):  # Tile at (x, y) in map coords
+    if (abs(x) < 5 and abs(y) < 5):
       return Tile.EMPTY
     else:
       return Tile.WALL
